b8b167d5ae14b85038ad6987b304e3a102d3743c
lahiru <lahirunirmalx@gmail.com>
2024-10-10 18:37:55 +0530

    creating orangehrm rector rule for testing

---
+++ src/rule/ConvertToMbstringRector.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OrangeHRM\Rector\Rule;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class ConvertToMbstringRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Replace standard string functions with mbstring equivalents',
+            [
+                new CodeSample(
+                    'strlen($string);',
+                    'mb_strlen($string, "UTF-8");'
+                ),
+            ]
+        );
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    public function refactor(Node $node): ?Node
+    {
+        if (!$node instanceof FuncCall) {
+            return null;
+        }
+
+        // Map of standard functions and their mbstring equivalents
+        $functionMapping = [
+            'strlen' => 'mb_strlen',
+            'substr' => 'mb_substr',
+            'strpos' => 'mb_strpos',
+            'strrpos' => 'mb_strrpos',
+            'strtolower' => 'mb_strtolower',
+            'strtoupper' => 'mb_strtoupper',
+            'substr_count' => 'mb_substr_count',
+        ];
+
+        // Check if the function is one of the standard functions we want to replace
+        $functionName = $this->getName($node);
+        if (!isset($functionMapping[$functionName])) {
+            return null;
+        }
+
+        // Replace with the mbstring equivalent
+        $node->name = new Name($functionMapping[$functionName]);
+
+        // Add the encoding parameter for mbstring functions if it's not already set
+        if (count($node->args) < 2) {
+            $node->args[] = $this->nodeFactory->createArg('UTF-8');
+        }
+
+        return $node;
+    }
+}
